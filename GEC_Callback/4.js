9f3583bb889fb6cc77d36ce99139b55e8bc02daf
Arnob Mondal <163873907+Arnobm45@users.noreply.github.com>
2024-07-19 14:37:30 +0530

    Create 4.js

---
+++ GEC_Callback/4.js
@@ -0,0 +1,16 @@
+{
+     console.log(x); // Undefined due to hosting
+     console.log(y); // throws Reference Error
+     console.log(z); // throws Reference Error
+
+var x = "Am";
+let y = "Hitman";
+const z ="!";
+
+
+    console.log(x); // Am
+    console.log(y); // Hitman 
+    console.log(z); // !
+
+
+}
